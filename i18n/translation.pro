bd28910124ab11d882a21a3098d8d261cf568a49
Mike <EZUSoft@users.noreply.github.com>
2016-06-28 21:43:55 +0200

    Add files via upload

---
+++ i18n/translation.pro
@@ -0,0 +1,10 @@
+FORMS = ../uiADXF2Shape.ui \
+        ../uiAbout.ui 
+SOURCES = ../__init__.py \
+ ../clsADXF2Shape.py \
+ ../clsDXFTools.py \
+ ../fnc4all.py \
+ ../uiADXF2Shape.py \
+ ../uiAbout.py
+
+TRANSLATIONS = translation_de.ts
---
+++ i18n/translation_de.ts
@@ -0,0 +1,293 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../clsDXFTools.py" line="127"/>
+        <source>Overwriting the following files</source>
+        <translation>Bestehende Dateien überschreiben</translation>
+    </message>
+    <message>
+        <location filename="../clsDXFTools.py" line="138"/>
+        <source>Error deleting file</source>
+        <translation>Datei(en) konnten nicht gelöscht werden</translation>
+    </message>
+    <message>
+        <location filename="../clsDXFTools.py" line="205"/>
+        <source>Cancel</source>
+        <translation>Abbruch</translation>
+    </message>
+    <message>
+        <location filename="../clsDXFTools.py" line="205"/>
+        <source>Pleace set target</source>
+        <translation>Zielverzeichnis festlegen</translation>
+    </message>
+    <message>
+        <location filename="../clsDXFTools.py" line="214"/>
+        <source>Edit Entity: </source>
+        <translation>Bearbeite Entity: </translation>
+    </message>
+    <message>
+        <location filename="../clsDXFTools.py" line="220"/>
+        <source>Creation &apos;</source>
+        <translation>Erstellung &apos;</translation>
+    </message>
+    <message>
+        <location filename="../fnc4all.py" line="136"/>
+        <source>Program termination</source>
+        <translation>Programmabbruch</translation>
+    </message>
+    <message>
+        <location filename="../fnc4all.py" line="136"/>
+        <source>Temporary directory
+</source>
+        <translation>Temporäres Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../fnc4all.py" line="136"/>
+        <source>can not be created</source>
+        <translation>konnte nicht erstellt werden</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../uiAbout.ui" line="14"/>
+        <source>EZUSoft: Another DXF Import / DXF2Shape Converter</source>
+        <translation>EZUSoft: DXF Import / DXF2Shape Konverter</translation>
+    </message>
+    <message>
+        <location filename="../uiAbout.ui" line="57"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;EZUSoft: &lt;/span&gt;
+DXF Import / DXF2Shape Converter &lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;
+&lt;span id=&quot;result_box&quot; class=&quot;short_text&quot; lang=&quot;en&quot;&gt;&lt;span&gt;Import&lt;/span&gt; &lt;span&gt;DXF files&lt;/span&gt; &lt;span&gt;
+and&lt;/span&gt; &lt;span&gt;optional&lt;/span&gt; &lt;span&gt;storage as&lt;/span&gt; &lt;span&gt;Shape&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Copyright (c) 2016 Mike Blechschmidt  &lt;a href=&quot;mailto:qgis@makobo.de&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;qgis@makobo.de&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;EZUSoft: &lt;/span&gt;
+DXF Import / DXF2Shape Konverter&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;
+&lt;span id=&quot;result_box&quot; class=&quot;short_text&quot; lang=&quot;en&quot;&gt;&lt;span&gt;Import&lt;/span&gt; &lt;span&gt;von DXF Dateien &lt;/span&gt;
+und optionale Speicherung als &lt;span&gt;Shape&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Copyright (c) 2016 Mike Blechschmidt  &lt;a href=&quot;mailto:qgis@makobo.de&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;qgis@makobo.de&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../uiAbout.ui" line="101"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;
+&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;ul style=&quot;margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; -qt-list-indent: 1;&quot;&gt;
+   &lt;li&gt;&lt;b&gt;Features:&lt;/b&gt;&lt;br&gt;- Other elements than the standard import are displayed
+&lt;br&gt;
+- Display of text (labeling)&lt;br&gt;- Categorization by Layer&lt;br&gt;
+- Group by Layer&lt;br&gt;- Separate representation by layers possible&lt;/span&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;
+
+
+
+&lt;a href=&quot;$$Homepage$$&quot;&gt;Homepage&lt;/a&gt;,&amp;nbsp;&amp;nbsp;
+&lt;a href=&quot;$$Forum$$&quot;&gt;Forum&lt;/a&gt;,&amp;nbsp;&amp;nbsp;
+&lt;a href=&quot;$$Doku$$&quot;&gt;Documentation&lt;/a&gt;,&amp;nbsp;&amp;nbsp;
+&lt;a href=&quot;https://github.com/EZUSoft/ADXF2Shape&quot;&gt;Code-Repository&lt;/a&gt;,&amp;nbsp; Lizenz: 
+&lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GPLv2&lt;/a&gt;
+&lt;/body&gt;
+&lt;/html&gt;
+</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;
+&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;ul style=&quot;margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; -qt-list-indent: 1;&quot;&gt;
+   &lt;li&gt;&lt;b&gt;Funktionalitäten:&lt;br&gt;&lt;/b&gt;- Mehr Elemente als beim Standardimport werden angezeigt&lt;br&gt;
+- Anzeige von Texten (Beschriftungen)&lt;br&gt;- Kategorisierung nach Layer&lt;br&gt;- Gruppierung nach Layer
+&lt;br&gt;
+- Separate Darstellung nach Layern möglich&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;
+
+
+
+&lt;a href=&quot;$$Homepage$$&quot;&gt;Homepage&lt;/a&gt;,&amp;nbsp;&amp;nbsp;
+&lt;a href=&quot;$$Forum$$&quot;&gt;Forum&lt;/a&gt;,&amp;nbsp;&amp;nbsp;
+&lt;a href=&quot;$$Doku$$&quot;&gt;Dokumentation&lt;/a&gt;,&amp;nbsp;&amp;nbsp;
+&lt;a href=&quot;https://github.com/EZUSoft/ADXF2Shape&quot;&gt;Code-Repository&lt;/a&gt;,&amp;nbsp; Lizenz: 
+&lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GPLv2&lt;/a&gt;
+&lt;/body&gt;
+&lt;/html&gt;
+</translation>
+    </message>
+</context>
+<context>
+    <name>clsADXF2Shape</name>
+    <message>
+        <location filename="../clsADXF2Shape.py" line="191"/>
+        <source>&amp;DXF Import/Convert</source>
+        <translation>&amp;DXF Import/Konvertierung</translation>
+    </message>
+    <message>
+        <location filename="../clsADXF2Shape.py" line="169"/>
+        <source>Import or Convert</source>
+        <translation>Importieren/Konvertieren</translation>
+    </message>
+    <message>
+        <location filename="../clsADXF2Shape.py" line="175"/>
+        <source>Documentation</source>
+        <translation>Dokumentation</translation>
+    </message>
+    <message>
+        <location filename="../clsADXF2Shape.py" line="181"/>
+        <source>About</source>
+        <translation>Über das Programm</translation>
+    </message>
+</context>
+<context>
+    <name>clsADXF2ShapeDialogBase</name>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="32"/>
+        <source>EZUSoft: Another DXF Import / DXF2Shape Converter</source>
+        <translation>EZUSoft: DXF Import / DXF2Shape Konverter</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="131"/>
+        <source>Input DXF-File</source>
+        <translation>DXF Quelldatei</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="149"/>
+        <source>select a DXF-file</source>
+        <translation>wähle eine DXF</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="188"/>
+        <source>.. per code ...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="195"/>
+        <source>Save as shape-files</source>
+        <translation>Shape-Dateien speichern</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="217"/>
+        <source>...code ...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="253"/>
+        <source>Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="291"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="69"/>
+        <source>use geometry collection</source>
+        <translation>Geometriecollektion
+(Blöcke/Signaturen)
+separat ausspielen</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="88"/>
+        <source>group layer
+(takes a long time)</source>
+        <translation>Layer einzeln gruppieren
+(zeitaufwändig)</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="304"/>
+        <source>Font size for labeling</source>
+        <translation>Textgröße für Beschriftungen</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.ui" line="233"/>
+        <source>Browse</source>
+        <translation>Durchsuche</translation>
+    </message>
+</context>
+<context>
+    <name>uiADXF2Shape</name>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="121"/>
+        <source>Map unit</source>
+        <translation>Karteneinheiten</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="122"/>
+        <source>Millimeter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="156"/>
+        <source>Specify destination path</source>
+        <translation>Zielpfad für Shapes festlegen</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="157"/>
+        <source>Output shape path</source>
+        <translation>Shape Zielpfad</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="199"/>
+        <source>DXF-file not selected</source>
+        <translation>DXF Datei nicht festgelegt</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="199"/>
+        <source>Please specify a DXF-File</source>
+        <translation>DXF Datei auswählen</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="202"/>
+        <source>DXF-file not found</source>
+        <translation>DXF-Datei nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="213"/>
+        <source>Destination path not selected</source>
+        <translation>Zielpfad nicht festgelegt</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="218"/>
+        <source>Destination path not found</source>
+        <translation>Zielpfad nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="229"/>
+        <source>Reset text height</source>
+        <translation>Texthöhe zurückgesetzt</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="225"/>
+        <source>Text height can not assume a zero value</source>
+        <translation>Texthöhe darf nicht Null sein</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="229"/>
+        <source>Error converting text to numbers</source>
+        <translation>Fehler bei der Umwandlung in eine Zahl</translation>
+    </message>
+    <message>
+        <location filename="../uiADXF2Shape.py" line="213"/>
+        <source>Please specify a target path for shapes</source>
+        <translation>Bitte das Zielverzeichnis fürdie Shapes festlegen</translation>
+    </message>
+</context>
+</TS>
